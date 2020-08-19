6e7e5880accfb13e39205837c616c9b3807d69dd
Sophia Lee <sophialee@sophias-mbp.lan>
2020-08-18 21:34:44 -0600

    clean up index.js

--- components/TripInfo/TripRow.jsx
+++ components/TripInfo/TripRow.jsx
@@ -3,20 +3,20 @@ import Link from 'next/link'
 
 const TripRow = (props) => {
 
-    return (
-      <div>
-        <li className={utilStyles.listItem} key={props.id}>
-            <div>
-                <img src={props.imageUrl} className={utilStyles.listImage}/>
-            </div>
-            <div className={utilStyles.listItemCopy}>
-                {props.name}
-                <br />
-                Region: {props.region}   
-            </div>
-        </li>
-      </div>
-    );
+  return (
+    <div>
+      <li className={utilStyles.listItem} key={props.id}>
+          <div>
+              <img src={props.imageUrl} className={utilStyles.listImage}/>
+          </div>
+          <div className={utilStyles.listItemCopy}>
+              {props.name}
+              <br />
+              Region: {props.region}   
+          </div>
+      </li>
+    </div>
+  );
 
 };
   
--- pages/index.js
+++ pages/index.js
@@ -2,31 +2,29 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import indexStyles from '../styles/index.module.css'
 
-export default function Home({ allTripsData }) {
+export default function Home() {
   return (
-    // <Layout>
-      <div className={indexStyles.pageContainer}>
-        <div className={indexStyles.textBody}>
-          <h2 className={utilStyles.headingLg}>Road Trip Advisor</h2>
-          <div>Want to take a road trip?</div>
-          <section className={utilStyles.headingMd}>Click one of the options below to start planning!</section>
-          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <button>
-              <Link href="explore">
-                Explore
-              </Link>
-            </button>
-            <button>
-              <Link href="create">
-                Create
-              </Link>
-            </button>
-          </section>
-        </div>
-        <div>
-          <img src="https://media.cntraveler.com/photos/5edfc029b16364ea435ca862/4:5/w_1600,h_2000,c_limit/Roadtrip-2020-GettyImages-1151192650.jpg" className={indexStyles.homeImage}></img>
-        </div>
+    <div className={indexStyles.pageContainer}>
+      <div className={indexStyles.textBody}>
+        <h2 className={utilStyles.headingLg}>Road Trip Advisor</h2>
+        <div>Want to take a road trip?</div>
+        <section className={utilStyles.headingMd}>Click one of the options below to start planning!</section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <button>
+            <Link href="explore">
+              Explore
+            </Link>
+          </button>
+          <button>
+            <Link href="create">
+              Create
+            </Link>
+          </button>
+        </section>
       </div>
-    // </Layout> 
+      <div>
+        <img src="https://media.cntraveler.com/photos/5edfc029b16364ea435ca862/4:5/w_1600,h_2000,c_limit/Roadtrip-2020-GettyImages-1151192650.jpg" className={indexStyles.homeImage}></img>
+      </div>
+    </div>
   )
 }
